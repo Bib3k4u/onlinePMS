29a76ab78c0fc5384d58076ac765872b97495c9a
Bibek <zhabibek4u@gmail.com>
2023-12-22 02:51:00 +0530

    Admin excel file updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: '#EAE7DC',
+        color2:'#D8C3A5',
+        color3:'#8E8D8A',
+        color4:'#E98074',
+        color5:'#E85A4F',
+
+      },
+    },
   },
   plugins: [],
 }
