f45b1e5d74811fe04ae31acdf0c71bfb39f725a0
cripttion <pulakshri@gmail.com>
2023-12-22 16:59:38 +0530

    excel uploading completed

--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const sequelize = require('./sequelize');
 const StudentList = require('./models/StudentList')(sequelize);
-
+const multer = require('multer');
+const xlsx = require('xlsx');
 const app = express();
 const port = process.env.PORT || 3001;
-const Excel = require('exceljs')
-const wb = new Excel.Workbook();
-const path = 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// Multer configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Get all students
 app.get('/students', async (req, res) => {
@@ -29,12 +32,46 @@ app.get('/students', async (req, res) => {
 });
 
 // Add a new student
-app.post('/students', async (req, res) => {
+app.post('/sdUpload', upload.single('file'), async (req, res) => {
   try {
-    const newStudent = await StudentList.create(req.body);
-    res.json(newStudent);
+    const buffer = req.file.buffer;
+    const wb = xlsx.read(buffer, { type: 'buffer' });
+    const sheetName = wb.SheetNames[0];
+    const sheet = wb.Sheets[sheetName];
+    const data = xlsx.utils.sheet_to_json(sheet);
+    await sequelize.sync();
+
+    await Promise.all(
+      data.map(async (row) => {
+        try {
+          // Assuming 'enrollmentNumber' is a unique identifier
+          const [student, created] = await StudentList.findOrCreate({
+            where: { enrollmentNumber: row.enrollmentNumber },
+            defaults: {
+              admissionNumber: row.admissionNumber,
+              nameOfStudent: row.nameOfStudent,
+              branch: row.branch,
+              year: row.year,
+              semester: row.semester,
+              contact: row.contact,
+              projectID: row.projectID,
+              createdAt: row.createdAt,
+              updatedAt: row.updatedAt,
+            },
+          });
+
+          // if (!created) {
+          //   console.log(`Entry with enrollment number ${row.enrollmentNumber} already exists. Skipping.`);
+          // }
+        } catch (error) {
+          console.error('Error adding student:', error);
+        }
+      })
+    );
+
+    res.json({ message: 'Data uploaded successfully' });
   } catch (error) {
-    console.error('Error adding student:', error);
+    console.error('Error adding students:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
