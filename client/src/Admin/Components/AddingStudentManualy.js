df4bbbefbed7e7a45f590e42a0be6e413533ae5a
Bibek <zhabibek4u@gmail.com>
2023-12-22 22:01:12 +0530

    a little correction in Admin

--- client/src/Admin/Components/AddingStudentManualy.js
+++ client/src/Admin/Components/AddingStudentManualy.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function AddingStudentManually() {
@@ -14,11 +14,41 @@ function AddingStudentManually() {
     projectId: '',
   });
 
+  const [semesters, setSemesters] = useState([]);
+  
+  useEffect(() => {
+    // Fetch semesters based on the selected year
+    if (studentData.year) {
+      // Assuming semesters are fetched from the server based on the selected year
+      // Adjust the API endpoint or fetch logic based on your server implementation
+      axios.get(`http://localhost:3001/semesters/${studentData.year}`)
+        .then(response => {
+          setSemesters(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching semesters:', error);
+        });
+    } else {
+      setSemesters([]);
+    }
+  }, [studentData.year]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
+
+    // Validation logic
+    let validatedValue = value;
+    if (name === 'enrollmentNumber' || name === 'year' || name === 'semester' || name === 'contact') {
+      validatedValue = parseInt(value, 10); // Convert to integer
+    }
+
+    if (name === 'admissionNumber' || name === 'branch' || name === 'nameOfStudent' || name === 'email' || name === 'projectId') {
+      // No specific validation for string fields
+    }
+
     setStudentData((prevData) => ({
       ...prevData,
-      [name]: value,
+      [name]: validatedValue,
     }));
   };
 
@@ -50,101 +80,123 @@ function AddingStudentManually() {
   };
 
   return (
-    <div>
-      <h2>Add Student Manually</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enrollment Number:
+    <div className="container mx-auto mt-8">
+      <h2 className="text-2xl font-bold mb-4 flex justify-center">Add Student Manually</h2>
+      <form onSubmit={handleSubmit} className="w-full  mx-auto">
+        <div className='flex flex-col md:flex-row lg:flex-row xl:flex-row justify-around'>
+        <label className="block mb-4">
+          <span className="text-gray-700">Enrollment Number:</span>
           <input
             type="text"
             name="enrollmentNumber"
             value={studentData.enrollmentNumber}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
 
-        <label>
-          Admission Number:
+        <label className="block mb-4">
+          <span className="text-gray-700">Admission Number:</span>
           <input
             type="text"
             name="admissionNumber"
             value={studentData.admissionNumber}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
 
-        {/* Repeat similar input fields for other properties */}
-        <label>
-          Name:
+        <label className="block mb-4">
+          <span className="text-gray-700">Name:</span>
           <input
             type="text"
             name="nameOfStudent"
             value={studentData.nameOfStudent}
             onChange={handleChange}
-          />
-        </label>
-        <label>
-        year
-          <input
-            type="number"
-            name="year"
-            value={studentData.year}
-            onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
 
-        <label>
-        branch
+        <label className="block mb-4">
+            <span className="text-gray-700">Year:</span>
+            <input
+              type="number"
+              name="year"
+              value={studentData.year}
+              onChange={handleChange}
+              className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
+            />
+          </label>
+
+        <label className="block mb-4">
+          <span className="text-gray-700">Branch:</span>
           <input
             type="text"
             name="branch"
             value={studentData.branch}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
-          
         </label>
+        </div>
 
-        <label>
-        semester
+        <div className='flex flex-col md:flex-row lg:flex-row xl:flex-row justify-around'>
+        <label className="block mb-4">
+          <span className="text-gray-700">Semester:</span>
           <input
             type="number"
             name="semester"
             value={studentData.semester}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
 
-        <label>
-        contact
+        <label className="block mb-4">
+          <span className="text-gray-700">Contact:</span>
           <input
             type="text"
             name="contact"
             value={studentData.contact}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
 
-
-        <label>
-        email
+        <label className="block mb-4">
+          <span className="text-gray-700">Email:</span>
           <input
             type="text"
             name="email"
             value={studentData.email}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
 
-        <label>
-        projectId
+        <label className="block mb-4">
+          <span className="text-gray-700">Project ID:</span>
           <input
             type="text"
             name="projectId"
             value={studentData.projectId}
             onChange={handleChange}
+            className="form-input mt-1 p-2 border rounded-md border-gray-400 block w-full"
           />
         </label>
-        <button type="submit">Add Student</button>
+
+        <label className=" flex justify-center items-center">
+        <span className="text-gray-700"></span>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-bold mt-1 w-60 h-12   rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+        >
+          Add Student
+        </button>
+        </label>
+
+        
+        </div>
       </form>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,14 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        color1: '#EAE7DC',
-        color2:'#D8C3A5',
-        color3:'#8E8D8A',
+        color1: '#6699CC',
+        color2:'#2A2A2A',
+        color3:'gray',
         color4:'#E98074',
         color5:'#E85A4F',
 
       },
     },
+  },
+  corePlugins: {
+    // ...
+    variants: true,
   },
   plugins: [],
 }
