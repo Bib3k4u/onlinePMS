cc478c6ca424ca871150a22583e05a89e8f58e42
Bibek <zhabibek4u@gmail.com>
2023-12-29 23:23:07 +0530

    bugs fixed and 404 designed

--- client/src/Admin/Components/AdminNav.js
+++ client/src/Admin/Components/AdminNav.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AdminNav = () => {
   return (
     
-    <div className='bg-bgColor shadow-lg text-textColor fixed top-0 w-full h-16 text-2xl tracking-wider items-center justify-center flex'>
+    <div className='bg-bgColor z-10 shadow-lg text-textColor fixed top-0 w-full h-16 text-2xl tracking-wider items-center justify-center flex'>
       Admin Portal
     </div>
   )
